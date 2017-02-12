5a96fa5fe256467e69ee74ebcc970f8e9673ad5e
tjd <timothyjamesdurrant@gmail.com>
2017-02-12 22:05:23 +1100

    assignment 3 updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Week 3 Assignment
+## Pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+##  makeCacheMatrix creates a special "matrix", a list containing a function to
+##  set the value of the matrix
+##  get the value of the matrix
+##  set the value of the inverse
+##  get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        inverseMatrix <- NULL
+        
+        set <- function(y){
+                x <<- y
+                inverseMatrix <<- NULL              
+        }
+        get <- function() x
+        getInverse <- function() inverseMatrix
+        setInverse <- function(inversed = NULL) inverseMatrix <<- inversed
+        list(set = set, get = get, getInverse = getInverse, setInverse = 
+                     setInverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special "matrix" returned by
+## makeCacheMatrix.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+        if(!is.null(m)) {
+                message("getting cached matrix")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data) #%*% data
+        x$setInverse(m)
+        m
 }
+
+#TEST CODE
+#assignmentWeek3 <- makeCacheMatrix()
+
+#assignmentWeek3$get()
+#assignmentWeek3$getInverse()
+#assignmentWeek3$setInverse()
+
+#testmatrix <- matrix(rnorm(16, 5), 4, 4)
+#assignmentWeek3$set(testmatrix)
+
+#cacheSolve(assignmentWeek3)
